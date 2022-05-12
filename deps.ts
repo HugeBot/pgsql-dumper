e29518ec07fc8ded9f27961bdb4d48cc169a60be
Juan Luis Caro <blademaker.live@gmail.com>
2022-05-12 15:16:04 +0000

    Remove typescript deno files

--- deps.ts
+++
@@ -1,3 +0,0 @@
-export { parse } from "https://deno.land/std@0.113.0/flags/mod.ts";
-export { encode } from "https://deno.land/std@0.113.0/encoding/base64.ts";
-export { readAll } from "https://deno.land/std@0.113.0/streams/conversion.ts";
