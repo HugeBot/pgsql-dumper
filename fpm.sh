0244d8cdf54a9f196f022bbdd4be6ea4480480bd
Juan Luis Caro <blademaker.live@gmail.com>
2022-09-04 20:30:17 +0200

    Do not add release tag to .deb names

--- fpm.sh
+++ fpm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Compiling AMD64 .deb"
-fpm --debug --no-depends -m BladeMaker -s dir -t deb -n psql-dumper -v $RELEASE_TAG -p bin/psql-dumper_${RELEASE_TAG}_amd64.deb bin/psql-dumper-x86_64-linux=/usr/local/bin/psql-dumper
+fpm --debug --no-depends -m BladeMaker -s dir -t deb -n psql-dumper -v $RELEASE_TAG -p bin/psql-dumper_amd64.deb bin/psql-dumper-x86_64-linux=/usr/local/bin/psql-dumper
 
 echo "Compiling i386 .deb"
-fpm --debug --no-depends -m BladeMaker -s dir -t deb -n psql-dumper -v $RELEASE_TAG -a i386 -p bin/psql-dumper_${RELEASE_TAG}_i386.deb bin/psql-dumper-i386-linux=/usr/local/bin/psql-dumper
+fpm --debug --no-depends -m BladeMaker -s dir -t deb -n psql-dumper -v $RELEASE_TAG -a i386 -p bin/psql-dumper_i386.deb bin/psql-dumper-i386-linux=/usr/local/bin/psql-dumper
