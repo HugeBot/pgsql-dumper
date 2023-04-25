fa3055f63bdf4e0f67d2d875c342c9ae6e5a9044
Blad3Mak3r <juanluiscarobenito@gmail.com>
2023-04-25 15:47:51 +0200

    chmod and use amd64

--- compile.sh
+++ compile.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
 echo "Compiling for Windows amd64..."
-GOOS=windows GOARCH=amd64 go build -o bin/pgsql-dumper-x86_64.exe -ldflags "-X main.Version=$RELEASE_TAG" main.go
+GOOS=windows GOARCH=amd64 go build -o bin/pgsql-dumper-amd64.exe -ldflags "-X main.Version=$RELEASE_TAG" main.go 
 
 echo "Compiling for Windows i386..."
 GOOS=windows GOARCH=386 go build -o bin/pgsql-dumper-i386.exe -ldflags "-X main.Version=$RELEASE_TAG" main.go
 
 echo "Compiling for Darwin amd64..."
-GOOS=darwin GOARCH=amd64 go build -o bin/pgsql-dumper-x86_64-darwin -ldflags "-X main.Version=$RELEASE_TAG" main.go
+GOOS=darwin GOARCH=amd64 go build -o bin/pgsql-dumper-amd64-darwin -ldflags "-X main.Version=$RELEASE_TAG" main.go
+chmod +x bin/pgsql-dumper-amd64-darwin
 
 echo "Compiling for Linux amd64..."
-GOOS=linux GOARCH=amd64 go build -o bin/pgsql-dumper-x86_64-linux -ldflags "-X main.Version=$RELEASE_TAG" main.go
+GOOS=linux GOARCH=amd64 go build -o bin/pgsql-dumper-amd64-linux -ldflags "-X main.Version=$RELEASE_TAG" main.go
+chmod +x bin/pgsql-dumper-amd64-linux
 
 echo "Compiling for Linux i386..."
 GOOS=linux GOARCH=386 go build -o bin/pgsql-dumper-i386-linux -ldflags "-X main.Version=$RELEASE_TAG" main.go
+chmod +x bin/pgsql-dumper-i386-linux
 
 echo "Done!!"
--- fpm.sh
+++ fpm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Compiling AMD64 .deb"
-fpm --debug --no-depends -m BladeMaker -s dir -t deb -n pgsql-dumper -v $RELEASE_TAG -p bin/pgsql-dumper_amd64.deb bin/pgsql-dumper-x86_64-linux=/usr/local/bin/pgsql-dumper
+fpm --debug --no-depends -m BladeMaker -s dir -t deb -n pgsql-dumper -v $RELEASE_TAG -p bin/pgsql-dumper_amd64.deb bin/pgsql-dumper-amd64-linux=/usr/local/bin/pgsql-dumper
 
 echo "Compiling i386 .deb"
 fpm --debug --no-depends -m BladeMaker -s dir -t deb -n pgsql-dumper -v $RELEASE_TAG -a i386 -p bin/pgsql-dumper_i386.deb bin/pgsql-dumper-i386-linux=/usr/local/bin/pgsql-dumper
